70efd639b1f31a0d34388ef888109e8f3782b28e
crearis <hans.doenitz@dasei.eu>
2025-10-11 19:49:36 +0200

    CHORE: remove wrong submodule

--- graphql_theaterpedia/graphql_vuestorefront/controllers/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2023 ODOOGAP/PROMPTEQUATION LDA
-# with edits from Hans Dönitz / theaterpedia.org
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import main
